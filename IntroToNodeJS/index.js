05cb0368d953e353c3141e2ddab649af0e991c77
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-03-19 16:24:06 +0530

    Learning about json web tokens

--- IntroToNodeJS/index.js
+++ IntroToNodeJS/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//import json-web-token module
+const jwt = require('jsonwebtoken');
 
 app.get('/', function(req, res){
     //by using req.json we can check the API response in the console or text editor(Need to install RESTClient extension in VS code)
     res.json({message: "Express is up and running"});
     // res.send('<h1>Express is up and running<h1>');
 })
+
+app.post('/tokenGenerate', (req, res) => {
+    const user = {
+        id: 1234,
+        username: 'Aug25B2022',
+        email: 'aug25B@coding.com'
+    }
+    jwt.sign(user, 'secret', function(err, token) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                token
+            })
+        }
+      });
+});
 
 app.listen(port, function(err){
     if(err){
